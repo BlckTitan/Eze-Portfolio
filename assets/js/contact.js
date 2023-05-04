a82bbab10514e299fce0455df5066a9f33f23afe
BlckTitan <dorshman406@gmail.com>
2023-05-04 17:20:43 +0100

    Validated the contact form input fields

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,4 +1,6 @@
-
+let name_error_message = document.querySelector('span.name');
+let email_error_message = document.querySelector('span.email');
+let message_error_message = document.querySelector('span.message');
 let submit_btn = document.querySelector('.submit');
 
 const service_id = 'service_opymzht';
@@ -14,8 +16,26 @@ const valdateForm = () =>{
         email: document.getElementById('email').value,
         message: document.getElementById('message').value
     }
+    let empty_field = '';
+    switch (empty_field) {
+        case params.name:
+            name_error_message.innerHTML = 'Name field cannot be empty';
+        break
+        case params.email:
+            email_error_message.innerHTML = 'Email field cannot be empty';
+        break;
+        case params.message:
+            message_error_message.innerHTML = 'Message field cannot be empty';
+        break;
     
-    handleSubmit(params)
+        default:
+            handleSubmit(params)
+            name_error_message.innerHTML = '';
+            email_error_message.innerHTML = '';
+            message_error_message.innerHTML = '';
+        break;
+    }
+    
 }
 const handleSubmit = (params) =>{
     
@@ -28,11 +48,21 @@ const handleSubmit = (params) =>{
         res => {
             document.getElementById('name').value = '',
             document.getElementById('email').value = '',
-            document.getElementById('message').value = ''
-        
-            console.log(res);
-            alert('message sent successfully')
-        }
+            document.getElementById('message').value = '';
+        },
+        handleSuccess()
+       
     ).catch((err) => console.log(err))
+}
+const handleSuccess = () => {
+    let success_container = document.querySelector('div.success');
+    let message_success = document.querySelector('span.success_message');
     
+    success_container.style.visibility = 'visible';
+    message_success.innerHTML = 'Email sent successfully';
+    
+    setInterval(() => {
+        message_success.innerHTML = '';
+        success_container.style.visibility = 'hidden';
+    }, 5000)
 }
